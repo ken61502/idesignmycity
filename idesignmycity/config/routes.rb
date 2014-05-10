f665a8a60c86a11310ca37cf7cc92fbd4d648c21
ken61502 <kennyson@gmail.com>
2014-05-10 15:43:49 +0800

    Draw Icon Touch JS Code

--- idesignmycity/config/routes.rb
+++ idesignmycity/config/routes.rb
@@ -6,7 +6,7 @@ Idesignmycity::Application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+    get 'draw' => 'draw_icon#draw'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
