1f59257993481ab3bc1d8cd1618dbead8673e2aa
Miles Wang <mw10104587@gmail.com>
2014-05-10 14:17:29 +0800

    new rails

---
+++ idesignmycity/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Idesignmycity::Application.config.secret_key_base = '1c4737ae2ecd3dd0d8f0d388dbd241c09266323b6252f8ca7d767d7ee03e5019e76a746842bee4eda1b0d9fcecbc4843a6f2ea290f972abcfa64600f64df818e'
---
+++ idesignmycity/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Idesignmycity::Application.config.session_store :cookie_store, key: '_idesignmycity_session'
